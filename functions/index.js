d9ff878e81b688598cbd5fcd7a268e368c715678
Dimitri PL <dimitrimachines@gmail.com>
2019-12-11 06:02:15 -0500

    config and user sign up

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,29 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-
-admin.initializeApp();
-
 const express = require("express");
 const app = express();
 
+admin.initializeApp();
+
+const config = {
+  apiKey: "AIzaSyBbqVc8GnWggwxu3GDykViihzjsZ_Ad1z0",
+  authDomain: "theory-35917.firebaseapp.com",
+  databaseURL: "https://theory-35917.firebaseio.com",
+  projectId: "theory-35917",
+  storageBucket: "theory-35917.appspot.com",
+  messagingSenderId: "974992797141",
+  appId: "1:974992797141:web:b7a1f2ff7583282283e15c",
+  measurementId: "G-M1YFHTWHKN"
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
 // Fetch all documents/posts in Waves collection.
 app.get("/waves", (request, response) => {
-  admin
-    .firestore()
-    .collection("waves")
+  db.collection("waves")
     .orderBy("createdAt", "desc")
     .get()
     .then(data => {
@@ -40,9 +53,7 @@ app.post("/wave", (request, response) => {
   };
 
   //Persist in database.
-  admin
-    .firestore()
-    .collection("waves")
+  db.collection("waves")
     .add(newWave)
     .then(doc => {
       response.json({ message: `document ${doc.id} created successfully` });
@@ -50,6 +61,42 @@ app.post("/wave", (request, response) => {
     .catch(err => {
       response.status(500).json({ error: "Something went wrong.." });
       console.error(err);
+    });
+});
+
+//User Sign Up Route.
+app.post("/signup", (request, response) => {
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    handle: request.body.handle
+  };
+
+  // Validate Data
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        return response
+          .status(400)
+          .json({ handle: "This handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return response.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use")
+      return response.status(500).json({ error: err.code });
     });
 });
 
