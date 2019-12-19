64167df7e8d3515026f5a39c75f7fe3a666b9e43
Dimitri PL <dimitrimachines@gmail.com>
2019-12-19 05:39:59 -0500

    validation error message fixed

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -36,7 +36,6 @@ exports.validateLoginData = data => {
   let errors = {};
   if (isEmpty(data.email)) errors.email = "Must not be empty";
   if (isEmpty(data.password)) errors.password = "Must not be empty";
-  if (Object.keys(errors).length > 0) return response.status(400).json(errors);
 
   return {
     errors,
